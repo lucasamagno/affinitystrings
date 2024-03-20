03f2cb27cbaaccb827602150e2f277c3b0304199
Lucas Magno <lucasamagno@gmail.com>
2024-03-19 20:37:21 -0400

    clean repo

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,13 @@
 <template>
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>HELLO</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
   }
 }
 </script>
-
-<style>
-html, body {
-  margin: 0;
-  padding: 0;
-  background-color: #040911;
-  font-family: 'Inter';
-}
-
-p {
-  opacity: 60%;
-}
-
-#app {
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #ffffff;
-}
-</style>
